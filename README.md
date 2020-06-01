c074d3b8be9a360b2cffc722f7c66f7beaa907ac
Roman Lutz <rolutz@microsoft.com>
2020-06-01 00:24:20 -0700

    add initial set of documents to get started
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # github-repo-stats
-Track stats for your repo over time!
+
+Track stats for your repo over time! GitHub tracks certain stats like commits,
+additions, deletions, and comments forever. Others like traffic data are only
+stored for two weeks. `github-repo-stats` solves this problem by providing a
+simple way to track your stats over time.
+
+The key idea here is to periodically run the script in this repo and store the
+results. On every run the previous results are updated in order to get a
+long-term view at the data.
+
+## How to set up github-repo-stats for your own repo
+
+To avoid having to manually run the script simply set up a new GitHub
+repository, configure a GitHub workflow to run the script and use the repo
+itself to store the results. Optionally, you can use GitHub Pages to visualize
+the results on a custom webpage.
+
+### Setting up a new GitHub repository
+
+Visit the [GitHub website](https://github.com) and click on the plus symbol in
+the top right corner next to your profile picture. Select "new repository" and
+provide an appropriate name, e.g. "<repo name>-stats".
+
+### Configure the GitHub workflow
+
+In your newly created repository go to "Settings", and then "Secrets". Add a
+token with permissions to the repo that you want stats on. To create a
+personal access token go to your profile, "Settings", "Developer settings",
+"Personal access tokens", and generate a new token. Make the token the least
+permissive, i.e., only provide read access. In detail this could mean:
+
+- read:discussion,
+- read:enterprise,
+- read:org,
+- read:packages,
+- read:repo_hook,
+- read:user,
+- repo
+
+We don't use all of these today (yet), but these settings should make your
+token future-proof for the purposes of this stats functionality.
+
+Copy your token and store it in your new repo under "Settings", "Secrets" as
+"ACCESS_TOKEN". Everyone who has permissions to access the secrets will now
+have access to your token, so be careful who you grant access.
+
+Navigate to "Actions" and copy-paste the contents of
+`github_workflow_template.yml` from this repository. Replace all occurrences
+of `<org>` with the organization of the repository you want to track, and
+`<repo>` with the repository itself.
+
+### Deploy a webpage to GitHub pages and visualize the results
+
+TODO
---
+++ collect_stats.py
@@ -0,0 +1,155 @@
+import argparse
+from collections import defaultdict
+import github
+import json
+import logging
+import os
+import sys
+
+
+_logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.INFO)
+
+
+def get_client(token):
+    if token is not None:
+        _logger.info("Received access token.")
+        access_token = token
+    
+    else:
+        with _LogWrapper("reading access token from local file"):
+            with open("access_token", "r") as access_token_file:
+                access_token = access_token_file.read()
+    
+    return github.Github(access_token)
+
+
+def write_stats_to_file(file_name, stats):
+    with open(file_name, 'w') as stats_file:
+        json.dump(stats, stats_file)
+
+
+def read_previous_stats_from_file(file_name):
+    if os.path.exists(file_name):
+        with open(file_name, 'r') as stats_file:
+            return json.load(stats_file)
+
+    _logger.warning(f"File {file_name} does not exist.")
+    return {}
+
+
+class _LogWrapper:
+    def __init__(self, description):
+        self._description = description
+
+    def __enter__(self):
+        _logger.info("Starting %s", self._description)
+
+    def __exit__(self, type, value, traceback):  # noqa: A002
+        # raise exceptions if any occurred
+        if value is not None:
+            raise value
+        _logger.info("Completed %s", self._description)
+
+
+def main(args):
+    parser = argparse.ArgumentParser(description="Collect GitHub repo stats")
+    parser.add_argument("--org", help="GitHub org", required=True)
+    parser.add_argument("--repo", help="GitHub repo", required=True)
+    parser.add_argument("--token", help="Access token", required=False)
+    parser.add_argument("--file-name", help="Stats file", required=True)
+    args = parser.parse_args(args)
+    org_name = args.org
+    repo_name = args.repo
+    token = args.token
+    file_name = args.file_name
+
+    with _LogWrapper("setting up GitHub client"):
+        client = get_client(token)
+
+    repo = client.get_repo(f"{org_name}/{repo_name}")
+
+    stats = {}
+
+    stats['stars'] = repo.stargazers_count
+
+    with _LogWrapper("collecting issues and associated comments"):
+        stats['open_issues'] = len(list(repo.get_issues(state='open')))
+        stats['closed_issues'] = len(list(repo.get_issues(state='closed')))
+
+        stats['created_issues'] = defaultdict(int)
+        stats['issue_comments'] = defaultdict(int)
+        for issue in repo.get_issues(state='all'):
+            stats['created_issues'][issue.user.login] += 1
+
+            for issue_comment in issue.get_comments():
+                stats['issue_comments'][issue_comment.user.login] += 1
+
+    with _LogWrapper("collecting traffic stats"):
+        stats['top_referrers'] = {
+            referrer.referrer: {
+                'count': referrer.count,
+                'uniques': referrer.uniques
+            }
+            for referrer in repo.get_top_referrers()
+        }
+
+        stats['clones_traffic'] = {
+            str(clone_record.timestamp): {
+                'count': clone_record.count,
+                'uniques': clone_record.uniques
+            }
+            for clone_record in repo.get_clones_traffic()['clones']
+        }
+
+        stats['views_traffic'] = {
+            str(view_record.timestamp): {
+                'count': view_record.count,
+                'uniques': view_record.uniques
+            }
+            for view_record in repo.get_views_traffic()['views']
+        }
+
+    with _LogWrapper("collecting contributor stats"):
+        stats['contributors'] = {
+            contributor.author.login: {
+                str(week.w): {
+                    "additions": week.a,
+                    "deletions": week.d,
+                    "commits": week.c
+                }
+                for week in contributor.weeks
+            }
+            for contributor in repo.get_stats_contributors()
+        }
+
+    with _LogWrapper("collecting pull request stats"):
+        stats['pull_requests'] = defaultdict(lambda: defaultdict(int))
+        for pull_request in repo.get_pulls(state='all', sort='created', base='master'):
+            author = pull_request.user.login
+            stats['pull_requests'][author]['created'] += 1
+
+            if pull_request.closed_at is None:
+                is_open = True
+            else:
+                is_open = False
+
+            stats['pull_requests'][author]['open'] += int(is_open)
+            stats['pull_requests'][author]['closed'] += 1 - int(is_open)
+
+        stats['pull_request_comments'] = defaultdict(int)
+        for comment in repo.get_pulls_comments():
+            stats['pull_request_comments'][comment.user.login] += 1
+
+    with _LogWrapper("reading previously recorded stats from file"):
+        combined_stats = read_previous_stats_from_file(file_name)
+
+    with _LogWrapper("combining old and new stats"):
+        combined_stats.update(stats)
+
+    with _LogWrapper("writing stats to a file"):
+        write_stats_to_file(file_name, combined_stats)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
